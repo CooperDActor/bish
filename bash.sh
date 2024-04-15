579d32a9e47f96801464008c89b23147cea53d4b
CooperDActor <136168153+CooperDActor@users.noreply.github.com>
2024-04-15 19:08:00 +1000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # update and upgrade
-sudo apt update
-sudo apt upgrade
-sudo apt full-upgrade
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt full-upgrade -y
 
 # Run Apps And Install em
-sudo apt install docker.io
+sudo apt install docker.io -y
 
 wget https://cloudron.io/cloudron-setup
 chmod +x ./cloudron-setup
